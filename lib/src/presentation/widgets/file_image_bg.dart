c498ea238d01e2610b21a818baefcca01e36da27
Sam <sam@hidaku.com>
2023-12-26 16:35:43 +0100

    file image widget updated

--- lib/src/presentation/widgets/file_image_bg.dart
+++ lib/src/presentation/widgets/file_image_bg.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api, no_leading_underscores_for_local_identifiers
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:image_pixels/image_pixels.dart';
 import 'package:poddin_moment_designer/src/domain/providers/notifiers/gradient_notifier.dart';
@@ -23,6 +24,23 @@ class FileImageBG extends StatefulWidget {
 class _FileImageBGState extends State<FileImageBG> {
   GlobalKey imageKey = GlobalKey();
   GlobalKey paintKey = GlobalKey();
+  final value = Random().nextInt(5);
+  final color1alignment = [
+    Alignment.topCenter,
+    Alignment.topRight,
+    Alignment.topLeft,
+    Alignment.centerLeft,
+    Alignment.center
+  ];
+  final color2alignment = [
+    Alignment.center,
+    Alignment.bottomLeft,
+    Alignment.centerRight,
+    Alignment.bottomCenter,
+    Alignment.bottomRight
+  ];
+  double imgHeight = double.infinity;
+  double imgWidth = double.infinity;
 
   @override
   void initState() {
@@ -32,19 +50,38 @@ class _FileImageBGState extends State<FileImageBG> {
   @override
   Widget build(BuildContext context) {
     final colorProvider = Provider.of<GradientNotifier>(context, listen: false);
+    var height = min(widget.dimension!.height, imgHeight);
+    var width = min(widget.dimension!.width, imgWidth);
+    //
     return RepaintBoundary(
       key: paintKey,
       child: ImagePixels(
         imageProvider: FileImage(File(widget.filePath!.path)),
         defaultColor: Colors.black,
         builder: (context, img) {
-          var color1 = img.pixelColorAtAlignment!(Alignment.topLeft);
-          var color2 = img.pixelColorAtAlignment!(Alignment.center);
+          var color1 =
+              img.pixelColorAtAlignment!(color1alignment.elementAt(value));
+          var color2 =
+              img.pixelColorAtAlignment!(color2alignment.elementAt(value));
+          //
+          imgHeight = img.hasImage
+              ? (img.height! > height)
+                  ? img.height! / 1.5
+                  : img.height! * 1.0
+              : imgHeight;
+          imgWidth = img.hasImage
+              ? (img.width! > width)
+                  ? img.width! / 1.5
+                  : img.width! * 1.0
+              : imgHeight;
+          setState(() {});
+          //
           colorProvider.color1 = color1;
           colorProvider.color2 = color2;
+          //
           return Image.file(
-            width: widget.dimension!.width * widget.scale!,
-            height: widget.dimension!.height * widget.scale!,
+            width: width * widget.scale!,
+            height: height * widget.scale!,
             File(widget.filePath!.path),
             key: imageKey,
             fit: BoxFit.cover,
