f49be4240a43e3a46c08dbaa0e2abe346960a6ef
Sam <sam@hidaku.com>
2023-12-26 01:23:23 +0100

    image background fixed

--- lib/src/presentation/widgets/file_image_bg.dart
+++ lib/src/presentation/widgets/file_image_bg.dart
@@ -3,20 +3,15 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_pixels/image_pixels.dart';
 import 'package:poddin_moment_designer/src/domain/providers/notifiers/gradient_notifier.dart';
-import 'dart:async';
-// ignore: unused_import
-import 'package:poddin_moment_designer/src/presentation/utils/color_detection.dart';
 import 'package:provider/provider.dart';
 
 class FileImageBG extends StatefulWidget {
   final File? filePath;
   final Size? dimension;
   final double? scale;
-  // final void Function(Color color1, Color color2) generatedGradient;
   const FileImageBG({
     super.key,
     required this.filePath,
-    // this.generatedGradient,
     required this.dimension,
     required this.scale,
   });
@@ -29,32 +24,8 @@ class _FileImageBGState extends State<FileImageBG> {
   GlobalKey imageKey = GlobalKey();
   GlobalKey paintKey = GlobalKey();
 
-  GlobalKey? currentKey;
-
-  final StreamController<Color> stateController = StreamController<Color>();
-
   @override
   void initState() {
-    // var colorProvider = Provider.of<GradientNotifier>(context, listen: false);
-    // currentKey = paintKey;
-    // Timer.periodic(const Duration(milliseconds: 200), (callback) async {
-    //   if (widget.scale! >= 1) {
-    //     var cd1 = await ColorDetection(
-    //       currentKey: currentKey,
-    //       paintKey: paintKey,
-    //       stateController: stateController,
-    //     ).searchPixel(Offset(widget.dimension!.width / 2.03, 480));
-    //     var cd2 = await ColorDetection(
-    //       currentKey: currentKey,
-    //       paintKey: paintKey,
-    //       stateController: stateController,
-    //     ).searchPixel(Offset(widget.dimension!.width / 2.03, 530));
-    //     colorProvider.color1 = cd1;
-    //     colorProvider.color2 = cd2;
-    //     callback.cancel();
-    //     stateController.close();
-    //   }
-    // });
     super.initState();
   }
 
@@ -63,24 +34,14 @@ class _FileImageBGState extends State<FileImageBG> {
     final colorProvider = Provider.of<GradientNotifier>(context, listen: false);
     return RepaintBoundary(
       key: paintKey,
-      // child: Image.file(
-      //   width: widget.dimension!.width * widget.scale!,
-      //   height: widget.dimension!.height * widget.scale!,
-      //   File(widget.filePath!.path),
-      //   key: imageKey,
-      //   fit: BoxFit.cover,
-      // ),
       child: ImagePixels(
         imageProvider: FileImage(File(widget.filePath!.path)),
         defaultColor: Colors.black,
         builder: (context, img) {
-          var color1 = img.pixelColorAtAlignment!(Alignment.center);
-          var color2 = img.pixelColorAtAlignment!(Alignment.topLeft);
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            colorProvider.color1 = color1;
-            colorProvider.color2 = color2;
-            setState(() {});
-          });
+          var color1 = img.pixelColorAtAlignment!(Alignment.topLeft);
+          var color2 = img.pixelColorAtAlignment!(Alignment.bottomRight);
+          colorProvider.color1 = color1;
+          colorProvider.color2 = color2;
           return Image.file(
             width: widget.dimension!.width * widget.scale!,
             height: widget.dimension!.height * widget.scale!,
